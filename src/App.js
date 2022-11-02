489c4d0f2a7f811279ba8fb1b274a68aa9fbc2d8
Jose Carlos Sáez Fernández <jcsaezfernandez@gmail.com>
2022-11-02 18:05:47 +0100

    iteration2 ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import foods from './foods.json';
+import { useState } from 'react';
+import FoodBox from './components/FoodBox';
+//console.log(foods)
 function App() {
+  const [foodList, setFoodList] = useState(foods);
+  // console.log("foodList", foodList)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {foodList.map((eachElement, index) => {
+          //console.log("foodList",eachElement)
+          return (
+             <FoodBox key={eachElement.name + index} foodList={eachElement}/>
+          );
+        })} ;
+      </div>
+     
+      
     </div>
   );
 }
---
+++ src/components/FoodBox.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+
+function FoodBox(props) {
+  console.log(props);
+  return (
+    <Card style={{ width: '18rem' }}>
+      <div>
+        <h3> {props.foodList.name} </h3>
+        <hr />
+        <div>
+          <img src={props.foodList.image} alt="food-dish" width="70px" />
+          <p>Calories: {props.foodList.calories}</p>
+          <p>Servings: {props.foodList.servings}</p>
+          <p>Total Calories: {props.foodList.calories}</p>
+          <Button>Delete</Button>
+
+        </div>
+      </div>
+    </Card>
+  );
+}
+
+export default FoodBox;

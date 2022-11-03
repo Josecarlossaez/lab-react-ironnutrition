c6b108925032c94782e6de625f76be8b63d344af
Jose Carlos Sáez Fernández <jcsaezfernandez@gmail.com>
2022-11-03 10:32:37 +0100

    iteration 7 modificado delete ok

--- src/App.js
+++ src/App.js
@@ -6,75 +6,73 @@ import { useState } from 'react';
 import FoodBox from './components/FoodBox';
 import AddFoodForm from './components/AddFoodForm';
 import Search from './components/Search';
+import Collapse from 'react-bootstrap/Collapse';
 //console.log(foods)
 function App() {
   const [foodList, setFoodList] = useState(foods);
   const [foodListSearch, setFoodListSearch] = useState(foods);
-  const [formIsShowing , setFormIsShowing] = useState(false)
-  
-  const addOneFood = (food) => {
-    
-    const copy = [...foodList]
-    copy.push(food)
-    setFoodList(copy)
+  const [formIsShowing, setFormIsShowing] = useState(false);
 
-    const copy2 = [...foodListSearch]
-    copy2.push(food)
-    setFoodListSearch(copy2)
-  }
+  const addOneFood = (food) => {
+    const copy = [...foodList];
+    copy.push(food);
+    setFoodList(copy);
+
+    const copy2 = [...foodListSearch];
+    copy2.push(food);
+    setFoodListSearch(copy2);
+  };
   const deleteFood = (foodName) => {
- const filteredList = foodList.filter((eachFood) => {
-  if(eachFood.name === foodName){
-    return false// no lo incluyas
-  }else {
-    return true// incluyelo
-  }
- })
- setFoodList(filteredList)
-  }
+    const filteredList = foodListSearch.filter((eachFood) => {
+      if (eachFood.name === foodName) {
+        return false; // no lo incluyas
+      } else {
+        return true; // incluyelo
+      }
+    });
+    setFoodListSearch(filteredList);
+  };
 
   const filterFoods = (filterQuery) => {
-    console.log("filterquery",filterQuery)
+    console.log('filterquery', filterQuery);
     const filteredArr = foodList.filter((eachFood) => {
-      return eachFood.name.toLowerCase().includes(filterQuery.toLowerCase())
-    })
-setFoodListSearch(filteredArr)
-  }
+      return eachFood.name.toLowerCase().includes(filterQuery.toLowerCase());
+    });
+    setFoodListSearch(filteredArr);
+  };
   const toggleForm = () => {
-    setFormIsShowing(formIsShowing === true ? false : true)
-  }
+    setFormIsShowing(formIsShowing === true ? false : true);
+  };
 
   return (
     <div className="App">
-    <div>
-    <h3>Food List</h3>
-    <Button type="submit" onClick={toggleForm}>Show Form</Button>
-    {formIsShowing === true && <AddFoodForm 
+      <h3>Food List</h3>
+      <Button type="submit" onClick={toggleForm}>
+        Show Form
+      </Button>
+      {/* {formIsShowing === true && <AddFoodForm 
      addOneFood={addOneFood}
      stateUpdater={setFoodList}
-    />}
-
-    </div>
-      {/* <AddFoodForm 
-      addOneFood={addOneFood}
-        stateUpdater={setFoodList}
-      /> */}
-      
-      <Search filterFoods={filterFoods}/>
-
-      <div style={{display:"flex",flexWrap:"Wrap", gap:"10px"}}>
-        {foodListSearch.map((eachElement, index) => {
-          
-          return (
-             <FoodBox key={eachElement.name + index} 
-             foodList={eachElement}
-              deleteFood={deleteFood}
-             />
-          );
-        })} ;
+    />} */}
+      <Collapse in={formIsShowing} >
+      <div>
+        <AddFoodForm addOneFood={addOneFood} stateUpdater={setFoodList} />
       </div>
-     
-     
+        </Collapse>
+      <Search filterFoods={filterFoods} />
+
+      <div style={{ display: 'flex', flexWrap: 'Wrap', gap: '10px' }}>
+        {foodListSearch.map((eachElement, index) => {
+          return (
+            <FoodBox
+              key={eachElement.name + index}
+              foodList={eachElement}
+              deleteFood={deleteFood}
+            />
+          );
+        })}{' '}
+        ;
+      </div>
     </div>
   );
 }
